61c322ad11280d048ce285ebbd9e5189f5dbdf44
paulosys <paulosilvasergio486@gmail.com>
2025-02-17 22:23:46 -0300

    Initial commit

--- src/main/java/org/example/EmitLogDirect.java
+++ src/main/java/org/example/EmitLogDirect.java
@@ -1,4 +1,24 @@
 package org.example;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
 public class EmitLogDirect {
+
+    private static final String EXCHANGE_NAME = "direct_logs";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+
+            String message = "Paulo Sérgio";
+
+            channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
 }
